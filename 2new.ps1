87677dfc07bca729e9588374712f7f43959dd9f1
Niraj Adhikari <67623286+hackThacker@users.noreply.github.com>
2023-08-29 10:23:42 +0545

    Add files via upload
    Certainly, I'd be happy to explain the code step by step:
    
    1. `$rootDirectory = Get-Location`: This line retrieves the current location or path where the script is executed and assigns it to the variable `$rootDirectory`.
    
    2. `function Move-FilesByExtension { ... }`: This line defines a PowerShell function named `Move-FilesByExtension` that takes a single parameter, `$rootDirectory`. The purpose of this function is to organize files in the specified directory based on their file extensions into different subfolders.
    
    3. `Get-ChildItem -Path $rootDirectory -Directory | ForEach-Object { ... }`: This part of the code gets a list of directories within the `$rootDirectory` and iterates through each one. It checks if the directory is located outside of the `$rootDirectory` and deletes it if so. This is done to clean up any stray directories that are not within the specified root.
    
    4. `$mainFolders = @('Images', 'Documents', 'Audio', 'Videos', 'Zip', 'Other')`: An array named `$mainFolders` is created, containing the names of the main folders where files will be organized.
    
    5. `$mainFolders | ForEach-Object { ... }`: This loop iterates through each main folder name in the `$mainFolders` array. It checks if each main folder exists within the `$rootDirectory` and creates it if it doesn't.
    
    6. `$fileExtensions = @{}`, `$logEntries = @{}`, and `$totalFilesMoved = 0`: These variables are initialized. `$fileExtensions` will be used to store file extensions, `$logEntries` to store log entries, and `$totalFilesMoved` to keep track of the number of files moved.
    
    7. `Get-ChildItem -Path $rootDirectory -File -Recurse | ForEach-Object { ... }`: This loop goes through each file within the `$rootDirectory` and its subdirectories. For each file, it determines the appropriate destination folder based on its file extension and moves the file to that folder.
    
       The code uses a series of `if` and `elseif` statements to categorize files into image, document, audio, video, zip, or other types, based on their extensions. For example, image files have extensions like 'jpg', 'png', 'gif', etc.
    
       It also handles cases where files with certain extensions need to be further organized into subfolders within the main folders, based on their extensions.
    
    8. Within each category of files, if a file has a valid extension, it moves the file to the appropriate subfolder, creates subfolders if needed, and logs the file move operation with a timestamp and details.
    
    9. If files have been moved, the script adds various log entries, like "Files moved successfully," "Total files moved," and "Logging process completed," to the log file.
    
    10. `Start-Sleep -Seconds 10` and `exit`: These lines make the script pause for 10 seconds before exiting. This might be intended to allow time to read the log messages before the script terminates.
    
    11. `Move-FilesByExtension -rootDirectory $rootDirectory`: This line actually calls the `Move-FilesByExtension` function, passing in the `$rootDirectory` variable that was set initially.
    
    In summary, the script is designed to organize files within a specified directory (`$rootDirectory`) into subfolders based on their file extensions. It categorizes files into main folders like Images, Documents, Audio, Videos, Zip, and Others, and then further organizes files within those categories into subfolders based on their extensions. Additionally, it logs the file move operations along with timestamps and details to a log file.

---
+++ 2new.ps1
@@ -0,0 +1,109 @@
+﻿$rootDirectory = Get-Location
+
+function Move-FilesByExtension {
+    param(
+        [string]$rootDirectory
+    )
+
+    # Delete additional directories found outside the root directory
+    Get-ChildItem -Path $rootDirectory -Directory | ForEach-Object {
+        if (-not $_.FullName.StartsWith($rootDirectory)) {
+            $null = Remove-Item -Path $_.FullName -Recurse -Force
+        }
+    }
+
+    # Create main folders if they don't exist
+    $mainFolders = @('Images', 'Documents', 'Audio', 'Videos', 'Zip', 'Other')
+    $mainFolders | ForEach-Object {
+        $folderPath = Join-Path -Path $rootDirectory -ChildPath $_
+        if (-not (Test-Path -Path $folderPath -PathType Container)) {
+            $null = New-Item -ItemType Directory -Path $folderPath -Force
+        }
+    }
+
+    $fileExtensions = @{}
+    $logEntries = @{}
+    $totalFilesMoved = 0
+
+    Get-ChildItem -Path $rootDirectory -File -Recurse | ForEach-Object {
+        $fileExtension = $_.Extension.TrimStart('.')
+        $destinationFolder = ''
+
+        if ($fileExtension -in @('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg', 'webp', 'ico', 'raw', 'psd', 'ai', 'eps', 'pcx', 'pict', 'tif', 'svgz', 'odg', 'epsf', 'eps2', 'eps3', 'jpe', 'jif', 'jfif', 'jfi', 'heif', 'heic', 'avif', 'bpg', 'jp2', 'j2k', 'jpf', 'jpx', 'jpm', 'mj2', 'jxr', 'wdp', 'hdp')) {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Images'
+        }
+        elseif ($fileExtension -in @('doc', 'docx', 'txt', 'rtf', 'pdf', 'ppt', 'pptx', 'xls', 'xlsx', 'csv', 'odt', 'ods', 'odp', 'odg', 'odf', 'pages', 'numbers', 'key', 'tex', 'xml', 'html', 'htm', 'mht', 'msg', 'eml', 'mbox', 'epub', 'fb2', 'azw', 'djvu', 'pdb')) {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Documents'
+        }
+        elseif ($fileExtension -in @('mp3', 'wav', 'wma', 'aac', 'flac', 'alac', 'm4a', 'ogg', 'opus', 'aiff', 'ape', 'dsd', 'mid', 'midi', 'amr', 'ac3', 'dts', 'ra', 'rm', 'mpc', 'mp+', 'mpc', 'mpp', 'mpga', 'mp1', 'mp2', 'm4r', 'm4b', 'm4p', 'm4v')) {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Audio'
+        }
+        elseif ($fileExtension -in @('mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', 'm4v', 'mpg', 'mpeg', '3gp', '3g2', 'm2v', 'rm', 'rmvb', 'vob', 'ts', 'mts', 'm2ts', 'divx', 'xvid', 'asf', 'swf', 'mpg4', 'mpeg4', 'ogv', 'f4v', 'h264', 'h265', 'hevc', 'rmhd')) {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Videos'
+        }
+        elseif ($fileExtension -in @('zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'xz', 'z', 'tgz', 'tbz', 'tbz2', 'txz', 'tlz', 'taz', 'tar.gz', 'tar.bz2', 'tar.xz', 'tar.lz', 'tar.lzma', 'tar.Z', 'tar.z', 'tar.Z', 'tar.z', 'zipx', 'jar', 'war', 'ear', 'apk', 'sit', 'sitx', 'ace', 'arj', 'cab', 'iso', 'dmg', 'vhd', 'vmdk', 'wim', 'xar', 'zoo', 'par', 'lzh', 'lha')) {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Zip'
+        }
+        else {
+            $destinationFolder = Join-Path -Path $rootDirectory -ChildPath 'Other'
+        }
+
+        if ($fileExtension) {
+            $subFolder = Join-Path -Path $destinationFolder -ChildPath $fileExtension
+            $null = New-Item -ItemType Directory -Path $subFolder -Force
+            $destinationPath = Join-Path -Path $subFolder -ChildPath $_.Name
+
+            # Check if the file already exists in the destination folder
+            if (-not (Test-Path -Path $destinationPath)) {
+                Move-Item -Path $_.FullName -Destination $destinationPath
+
+                $timestamp = Get-Date -Format 'hh:mm:ss tt'
+                $date = Get-Date -Format 'yyyy-MM-dd'
+                $day = Get-Date -Format 'dddd'
+
+                $logEntry = "$timestamp - $date ($day): Moved $($_.Name) to $destinationFolder\$fileExtension"
+
+                # Check if the log entry has already been logged
+                if (-not $logEntries.ContainsKey($logEntry)) {
+                    $logEntries[$logEntry] = $true
+                    $totalFilesMoved++
+                    
+                    # Append log entry to log file
+                    $logPath = Join-Path -Path $rootDirectory -ChildPath 'log.txt'
+                    if (Test-Path -Path $logPath) {
+                        $logEntry | Out-File -FilePath $logPath -Append -Encoding utf8
+                    }
+                    else {
+                        $logEntry | Out-File -FilePath $logPath -Encoding utf8
+                    }
+                }
+            }
+        }
+    }
+
+    if ($totalFilesMoved -gt 0) {
+        $successMessage = "Files moved successfully."
+        $logEntries[$successMessage] = $true
+
+        $totalFilesMovedMessage = "Total files moved: $totalFilesMoved"
+        $logEntries[$totalFilesMovedMessage] = $true
+
+        $logCompletedMessage = "Logging process completed."
+        $logEntries[$logCompletedMessage] = $true
+
+        # Add two new lines
+        $logEntries["`r`n"] = $true
+        $logEntries["`r`n"] = $true
+
+        # Write log entries to the log file
+        $logEntries.Keys | Out-File -FilePath $logPath -Append -Encoding utf8
+    }
+}
+
+
+# Example usage
+Move-FilesByExtension -rootDirectory $rootDirectory
+
+# Close the script after 10 seconds
+Start-Sleep -Seconds 10
+exit
